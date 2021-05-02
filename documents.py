e7240b5a18ee7d4e642ae04a84ee19a1780f77a0
Punit Goswami <pntgoswami18@gmail.com>
2021-05-02 23:29:34 +0530

    bug, added relevance handling

--- documents.py
+++ documents.py
@@ -1,14 +1,22 @@
 from collections import Counter
-from dataclasses import  dataclass
+from dataclasses import dataclass
+
+from .analysis import analyze
 
 @dataclass
 class Abstract:
-    ''' Wikipedia Abstract '''
+    """Wikipedia abstract"""
     ID: int
     title: str
     abstract: str
-    URL: str
+    url: str
 
     @property
     def fulltext(self):
         return ' '.join([self.title, self.abstract])
+
+    def analyze(self):
+        self.term_frequencies = Counter(analyze(self.fulltext))
+
+    def term_frequency(self, term):
+        return self.term_frequencies.get(term, 0)
